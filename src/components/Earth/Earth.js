447142d2f259995e43cf9998f31c2f7fc62d87e0
Brian Howell <brianhowell@pm.me>
2023-12-18 09:19:38 -0500

    Earth > moon > craters

--- src/components/Earth/Earth.js
+++ src/components/Earth/Earth.js
@@ -5,7 +5,11 @@ const Earth = () => {
   return (
     <div id="earth" className="planetary-system">
       <div className="planet"></div>
-      <div className="moon"></div>
+      <div className="moon">
+        <div id="crater1" className="crater"></div>
+        <div id="crater2" className="crater"></div>
+        <div id="crater3" className="crater"></div>
+      </div>
     </div>
   );
 };
