6b2922ab29a704105162f10ec6ef33cf85147973
Brian Howell <brianhowell@pm.me>
2023-12-20 14:15:48 -0500

    Bug fix: Prevent moon animation from affecting the y-position of the planet description below.

--- src/components/Earth/Earth.js
+++ src/components/Earth/Earth.js
@@ -5,18 +5,19 @@ const Earth = () => {
   return (
     <div id="earth" className="planetary-system">
       <div className="planet"></div>
-      <div className="moon">
-        <div id="crater1" className="crater"></div>
-        <div id="crater2" className="crater"></div>
-        <div id="crater3" className="crater"></div>
+      <div className="moon-container">
+        <div className="moon">
+          <div id="crater1" className="crater"></div>
+          <div id="crater2" className="crater"></div>
+          <div id="crater3" className="crater"></div>
+        </div>
       </div>
       <div className="planet-desc">
         <h2>Earth</h2>
         <p>Our Home Planet</p>
         <p className="css-note">
-          <span>CSS Technique:</span> The moon's orbit is animated using the top
-          and left positions. Z-index layers the moon behind the planet as
-          needed.
+          <span>CSS Technique:</span> The moon's orbit is animated using the
+          left position. Z-index layers the moon behind the planet as needed.
         </p>
       </div>
     </div>
